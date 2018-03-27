03fe9bc2e1037f537ae2b355cb86eb27d841d6ac
abtExp <abt.exp@gmail.com>
2018-03-27 20:14:24 +0530

    version 0.5 release

--- test/classTest.js
+++ test/classTest.js
@@ -1,12 +1,28 @@
+/**
+ * @class Names @extends Enter 
+ */
 export default class Names extends Enter {
+    /**
+     * @constructor
+     */
     constructor(a, b) {
         console.log(a, b);
     }
 
+
+    /**
+     * @method methodsName
+     * @param {any} some - none
+     * @param {any}  def - none
+     */
     methodsName(some, def) {
 
     }
 
+    /**
+     * @method defFunc
+     * @param {any} asd - none
+     */
     defFunc(asd) {
 
     }
--- test/testFile.js
+++ test/testFile.js
@@ -1,3 +1,16 @@
+/**
+ * @function named
+ * @param {any} name - none
+ * @param {any}  classed - none
+ */
 function named(name, classed) {
+
+}
+
+/**
+ * @function def
+ * @param {any} avc - none
+ */
+function def(avc) {
 
 }
