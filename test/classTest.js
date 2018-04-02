ac5349a14fbdff158dd9ddd35c10d6cfae211f7e
abtExp <abt.exp@gmail.com>
2018-04-02 07:47:32 +0530

    fixed constructor param capture

--- test/classTest.js
+++ test/classTest.js
@@ -2,27 +2,28 @@
  * @class Names @extends Enter 
  */
 export default class Names extends Enter {
-    /**
-     * @constructor
-     */
+/**
+ * @constructor 
+ * @param {any} a - none
+ * @param {any}  b - none
+ */
     constructor(a, b) {
         console.log(a, b);
     }
 
-
-    /**
-     * @method methodsName
-     * @param {any} some - none
-     * @param {any}  def - none
-     */
+/**
+ * @method methodsName
+ * @param {any} some - none
+ * @param {any}  def - none
+ */
     methodsName(some, def) {
 
     }
 
-    /**
-     * @method defFunc
-     * @param {any} asd - none
-     */
+/**
+ * @method defFunc
+ * @param {any} asd - none
+ */
     defFunc(asd) {
 
     }
