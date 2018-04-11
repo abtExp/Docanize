486e9382d892fb931633f7277aa25ed7268008b3
abtExp <abt.exp@gmail.com>
2018-04-11 16:03:07 +0530

    added yargs dir options

--- test/test2/testFile.js
+++ test/test2/testFile.js
@@ -3,6 +3,23 @@ function named(name, classed) {
 }
 
 function def(avc) {
+    while(true){
+        console.log('hahahaha!!!');
+    }
+
+    if(conf) console.log('asd');
+    for(const i in mapped){
+        console.log(i);
+    }
+
+    arr.map(i=>{
+        i*=2;
+    })
+
+    arr.forEach(i=>{
+        i *=3;
+    })
+
     return avc;
 }
 
